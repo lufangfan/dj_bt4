1acbdf6c10d77201b096227d90547d080c786c35
lufangfan <lufangfan_py@163.com>
2018-08-20 17:52:23 +0800

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+@login_required
+def myaccount(request):
+    return HttpResponse('my account')
+
+
+
