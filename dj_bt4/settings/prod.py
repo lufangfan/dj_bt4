33ab0c764120143407d7a71878e16ad2a4ca4e79
lufangfan <lufangfan_py@163.com>
2018-11-28 15:35:24 +0800

    添加ALLOWED_HOSTS

--- dj_bt4/settings/prod.py
+++ dj_bt4/settings/prod.py
@@ -1,7 +1,7 @@
 from dj_bt4.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['132.232.47.178', 'www.lufangfan.xyz', 'lufangfan.xyz']
+ALLOWED_HOSTS = ['www.lufangfan.xyz', 'lufangfan.xyz']
 
 #  MyProject.settings.py 里面设置MEDIA_ROOT and MEDIA_URL
 STATIC_ROOT = "/home/ubuntu/www/lufangfan.xyz/static/"
