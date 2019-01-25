7709619da897350772f6ba499d222485365c627f
lufangfan <36727697+lufangfan@users.noreply.github.com>
2019-01-25 16:59:26 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_bt4.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_bt4.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
