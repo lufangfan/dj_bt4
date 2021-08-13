b0f0c39888865de58ec5ac9809cbcac8230ab1b6
lufangfan <36727697+lufangfan@users.noreply.github.com>
2021-08-13 22:39:58 +0800

    Update prod.py
    允许访问地址

--- dj_bt4/settings/prod.py
+++ dj_bt4/settings/prod.py
@@ -1,7 +1,7 @@
 from dj_bt4.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['www.lufangfan.xyz', 'lufangfan.xyz']
+ALLOWED_HOSTS = ['www.lufangfan.xyz', 'lufangfan.xyz', '127.0.0.1']
 
 #  MyProject.settings.py 里面设置MEDIA_ROOT and MEDIA_URL
 STATIC_ROOT = "/home/ubuntu/www/lufangfan.xyz/static/"
